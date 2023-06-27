9a41ba7056a81c0b80f3dab2fb5808d0bb3f1ec8
Keeanu Rodriguez <keeanu7@gmail.com>
2023-06-27 18:45:52 -0400

    fixes test and passes insert

--- solution.js
+++ solution.js
@@ -1,5 +1,31 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor (data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor (head = null) {
+    this.head = head
+  }
+  insert (data) {
+    this.data = data
+    if (!this.head) {
+      this.head = new Node(this.data);
+      return this.head;
+    }
+    let currentNode = this.head;
+
+    while(currentNode.next){
+      currentNode = currentNode.next;
+    }
+    currentNode.next = new Node(this.data);
+  }
+}
+
 module.exports = {
   Node,
   LinkedList,
