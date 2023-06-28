138785fa0f9de53cc2f8260ce9569d11da0855e3
amber-king <amberking@pursuit.org>
2023-06-28 11:31:32 -0400

    insert method added & working

--- solution.js
+++ solution.js
@@ -1,15 +1,33 @@
 const { nums, words } = require("./data/data.js");
 
+// create a Node class w/ properties
+// properties of data & next
 class Node {
   constructor(data) {
     this.data = data;
-    this.next.null;
+    this.next = null;
   }
 }
 
+// create a linked list w/ properties
+// properties of head
 class LinkedList {
   constructor(head = null) {
     this.head = head;
+  }
+
+  // Linked Liast Methods
+
+  // insert() method - add to the linked list using an insert method
+  // can create a new node, new linked list
+  insert(data) {
+    let newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
   }
 }
 module.exports = {
