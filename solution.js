b825c35bfd567f82f9bd5e75e6516992107cac38
elizabethbright721 <elizabethbright721@gmail.com>
2023-07-10 09:51:15 -0400

    create node class, linked list with properties, creat methods insert,size, delete, getFirst, getLast, and search

--- solution.js
+++ solution.js
@@ -1,5 +1,73 @@
 const { nums, words } = require("./data/data.js");
+ class Node {
+  constructor(data){
+  this.data = data;
+  this.next = null;
+  }
+}
 
+class LinkedList {
+  constructor(){
+    this.head = null;
+    this.tail = null;
+  }
+  insert(data){
+    let newNode = new Node(data);
+    if(!this.head){
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+  size(){
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next
+    }
+    return count;
+  }
+  delete(data){
+    let node = this.head;
+    let counter = 0;
+    while (node.data !==data && node.next){
+      counter++;
+      node = node.next;
+    }
+    let foundNode = node;
+    node = this.head;
+    for(let i = 1; i < counter; i++){
+      node = node.next
+    }
+    node.next = foundNode.next;
+  }
+  getFirst(){
+    return this.head;
+  }
+  getLast(){
+    return this.tail;
+  }
+  search(key){
+    let node = this.head;
+    while (node !== null && node.data !== key){
+      node.node.next;
+    }
+    return node;
+  }
+  getKth(){
+    
+  }
+  getKthToLast(){}
+  isEmpty(){}
+  clear(){
+    this.head = null;
+  }
+  toArray(){}
+  containsDuplicates(){}
+
+}
 module.exports = {
   Node,
   LinkedList,
