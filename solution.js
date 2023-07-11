57cd23894d295e3ee6ceb25dc1259d4b3dbe5e70
elizabethbright721 <elizabethbright721@gmail.com>
2023-07-11 12:33:04 -0400

    Completed Lab

--- solution.js
+++ solution.js
@@ -1,25 +1,29 @@
 const { nums, words } = require("./data/data.js");
- class Node {
-  constructor(data){
-  this.data = data;
-  this.next = null;
+ 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   }
 }
 
 class LinkedList {
-  constructor(){
+  constructor() {
     this.head = null;
     this.tail = null;
   }
+
   insert(data){
     let newNode = new Node(data);
     if(!this.head){
       this.head = newNode;
+      this.tail = newNode;
     } else {
-      newNode.next = this.head;
-      this.head = newNode;
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
   }
+
   size(){
     let count = 0;
     let node = this.head;
@@ -29,10 +33,11 @@ class LinkedList {
     }
     return count;
   }
+
   delete(data){
     let node = this.head;
     let counter = 0;
-    while (node.data !==data && node.next){
+    while (node.data !== data && node.next){
       counter++;
       node = node.next;
     }
@@ -43,28 +48,62 @@ class LinkedList {
     }
     node.next = foundNode.next;
   }
+
   getFirst(){
     return this.head;
   }
+
   getLast(){
     return this.tail;
   }
+
   search(key){
     let node = this.head;
     while (node !== null && node.data !== key){
-      node.node.next;
+      node = node.next;
     }
     return node;
   }
-  getKth(){
-    
+
+  getKth(k) {
+    let node = this.head;
+    let index = 0;
+    while (node && k !== index) {
+      node = node.next;
+      index++;
+    }
+    return node;
   }
-  getKthToLast(){}
-  isEmpty(){}
+
+  getKthToLast(k) {
+    let node = this.tail;
+    let index = 0;
+    while (node) {
+      if ((this.size() - k) !== index) return node
+      node = node.next;
+      index++;
+    }
+  }
+
+  isEmpty(){
+    return (this.head === null && this.tail === null);
+  }
+
   clear(){
     this.head = null;
+    this.tail = null;
   }
-  toArray(){}
+
+  toArray(){
+    let node = this.head;
+    let arr = []
+    while (node !== null){
+      arr.unshift(node.data);
+      node = node.next
+    }
+    return arr;
+  }
+
   containsDuplicates(){}
 
 }
