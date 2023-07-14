17df325468005e763bc6657437a6169ac84e0429
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-07-13 20:29:57 -0400

    ✨ Creates Node Class and Linked List class with Insert Method

--- solution.js
+++ solution.js
@@ -1,5 +1,32 @@
 const { nums, words } = require("./data/data.js");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+  setNext(val) {
+    this.next = val;
+  }
+}
+
+class LinkedList {
+  constructor(head) {
+    this.head = head;
+    this.tail = null;
+  }
+  insert(node) {
+    let newNode = new Node(node);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.head.setNext(newNode);
+    }
+    this.head = newNode;
+  }
+}
+
 module.exports = {
   Node,
   LinkedList,
